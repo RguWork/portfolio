58a14591af5fbe9c9756d11b9b6c88b721f76f2f
R Gu <rguwork9988@gmail.com>
2024-07-24 22:08:37 -0400

    more bug fixing images

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Python",
     description:
       "Developed a navigation tool leveraging Deep Convolutional Neural Networks (ResNet-34) for MIT's underground tunnels, enabling navigation with 99.66% precision through complex architectures and enhancing the tunnels as a valuable commuting resource for students.",
-    image: "/public/images/tunnels.webp",
+    image: "/portfolio/public/images/tunnels.webp",
     link: "https://github.com/RguWork/TunnelsNavApp",
   },
 ];
@@ -15,7 +15,7 @@ export const research = [
     subtitle: "Python and Excel",
     description:
       "Leading research on generative AI models to optimize audio-induced water extraction from polymers, focusing on maximizing efficiency while reducing energy requirements, enhancing water accessibility and simplifying transportation.",
-    image: "/public/images/hydrogel_pca.png",
+    image: "/portfolio/public/images/hydrogel_pca.png",
     link: "https://github.com/RguWork/HydrogelGenAI",
   },
   {
@@ -23,7 +23,7 @@ export const research = [
     subtitle: "Python",
     description:
       "Developed novel generative AI methods utilizing equivariance in conjunction with generative models such as Generative Adversarial Networks (GANs), allowing for preservation of detailed structural information and accelerated learning for textile data. Paper pending publication on the linked website.",
-    image: "/public/images/lace.png",
+    image: "/portfolio/public/images/lace.png",
     link: "https://mit-genai.pubpub.org/",
   },
   {
@@ -31,7 +31,7 @@ export const research = [
     subtitle: "Python",
     description:
       "Conducted research on Hyperspectral Mask Regional Neural Networking to create a deep learning model that optimizes methane detection within hyperspectral images, with an 87% success rate improvement. Published and presented research paper at 2021 IEEE 3rd ICFTFC.",
-    image: "/public/images/methane.png",
+    image: "/portfolio/public/images/methane.png",
     link: "https://ieeexplore.ieee.org/abstract/document/9647136",
   },
 ];
@@ -42,7 +42,7 @@ export const miscellaneous = [
     subtitle: "Music Theory",
     description:
       "Composed a string quartet piece called 'There are Gnomes in the Forest' as a final piece for a music theory class.",
-    image: "/public/images/gnomes.png",
+    image: "/portfolio/public/images/gnomes.png",
     link: "https://soundcloud.com/derek-david-1/1-07gu-there-are-gnomes-in-the/s-PelMGrgPbDp?in=derek-david-1/sets/fall2023mus301//s-BZrFYZlZF8j",
   },
   {
@@ -50,7 +50,7 @@ export const miscellaneous = [
     subtitle: "Diving",
     description:
       "Ended 1st at the 2020 PSAL high school diving championship with a DD of 513.9.",
-    image: "/public/images/psal.JPG",
+    image: "/portfolio/public/images/psal.JPG",
     link: "https://www.riverdalepress.com/stories/bronx-sciences-gu-dives-his-way-to-the-top,71436",
   },
 ];
